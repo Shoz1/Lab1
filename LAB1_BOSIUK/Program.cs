a202d1d701694182c7f3a672b8de33fe9e4e0e04
Shoz1 <48013643+Shoz1@users.noreply.github.com>
2019-03-10 16:32:45 +0200

    Update Program.cs

--- LAB1_BOSIUK/Program.cs
+++ LAB1_BOSIUK/Program.cs
@@ -1,12 +1,20 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-namespace LAB1_BOSIUK
+namespace ConsoleApplication1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter first number and press enter: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Enter second number and press enter: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("result=" + (a*b));
         }
+
     }
 }
